6dcbc9474de4863ddc46796e60abcf7d75f0167d
Santthosh <santthosh@appinmap.com>
2012-11-06 03:27:24 -0800

    Adding production configuration

---
+++ config/deploy/production.rb
@@ -0,0 +1,7 @@
+set :branch, "master"
+
+role :web, "ec2-54-245-39-219.us-west-2.compute.amazonaws.com"                          # Your HTTP server, Apache/etc
+role :app, "ec2-54-245-39-219.us-west-2.compute.amazonaws.com"                          # This may be the same as your `Web` server
+
+ssh_options[:user] = "ubuntu"
+ssh_options[:keys] = ["/data/ops/aws-keys/us-west-oregon/brightpush-workers.pem"]
