cabd846de77a5c5a1d0d3ffae95967955797a309
Santthosh <santthosh@appinmap.com>
2012-11-06 04:08:25 -0800

    Setting the rack_env

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,5 +3,7 @@ set :branch, "master"
 role :web, "ec2-54-245-39-219.us-west-2.compute.amazonaws.com"                          # Your HTTP server, Apache/etc
 role :app, "ec2-54-245-39-219.us-west-2.compute.amazonaws.com"                          # This may be the same as your `Web` server
 
+set :rack_env,"production"
+
 ssh_options[:user] = "ubuntu"
 ssh_options[:keys] = ["/data/ops/alpha/aws-keys/us-west-oregon/brightpush-workers.pem"]
